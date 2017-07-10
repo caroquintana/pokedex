4a9c374da15aeae097ffd440eb4c67bb3a7947d7
caroquintana <caro.quintana.n@gmail.com>
2017-07-09 21:55:34 -0400

    insertar datos en el modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  //llamada ajax
     $.ajax({
     url: 'http://pokeapi.co/api/v2/pokemon/',
     type: 'GET',
@@ -7,10 +7,29 @@ $(document).ready(function() {
     data: {"limit":"811"},
     })
     .done(function(response) {
-        for(var i=1; i <= 100; i++){
+        for(var i=1; i <= 50; i++){ //solo traigo 50 pokemones para que no colapse el github
+          //aquí inserto en el div las imagenes de los pokemones aplicandole una clase de materialize
         $('#todospokemones').append("<a class='waves-effect waves-light btn' href='#modal1'><img id='"+i+"' src='http://pokeapi.co/media/img/"+i+".png'></a>");
         }
-        
+        $('#todospokemones').on('click','img',function(){
+            var id = $(this).attr('id'); //darle el atributo id al pokemon
+            //La función $.get() solo provee una función para saber que la operación fue exitosa.
+            $.get('http://pokeapi.co/api/v2/pokemon/' + id, function(pokemon){
+                var insertarInfo = "";
+                insertarInfo += "<h1>" + pokemon.name.toUpperCase() + "</h1>"; //que el nombre del pokemon quede en mayuscula
+                insertarInfo += "<p><img src='http://pokeapi.co/media/img/" + pokemon.id + ".png'></p>"; //llamando a la imagen dentro del modal según id
+                insertarInfo += "<h2>Tipo</h2>"; //
+                for(var i = 0; i < pokemon.types.length; i++) { // bucle que me trae el tipo del pokemon del json
+                    insertarInfo += "<p>" + pokemon.types[i].type.name + "</p>"; //y lo inserta en una etiqueta p
+                }
+                insertarInfo += "<h4>Mide</h4>";
+                insertarInfo += "<p>" + pokemon.height + " metros." + "</p>"; //traeme la altura
+                insertarInfo += "<h4>Pesa</h4>";
+                insertarInfo += "<p>" + pokemon.weight + " kilos." + "</p>"; //traeme el peso
+                $('.modal-content').html(insertarInfo); //inserta todo esto en el modal-content
+            }, "json");
+
+        }); 
 
     })
     .fail(function() {
@@ -23,7 +42,7 @@ $(document).ready(function() {
 });
 
 
-
+//config de modal según materialize
   $('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
